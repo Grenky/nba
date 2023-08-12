8684926efa3e5310ab807c5642b24f085497f3e1
Grenky <demosoft62@gmail.com>
2023-08-12 11:29:55 +0300

    13

--- src/components/UserAccount.jsx
+++ src/components/UserAccount.jsx
@@ -5,6 +5,7 @@ export default function UserAccount() {
 
     const[userName, setUserName] = useState('User Name');
     const[avatar, setAvatar] = useState('../images/useravatar.png');
+    const[userDescription, setUSerDescription] = useState('');
 
     const handleNameChange = (event) => {
         setUserName(event.target.value);
@@ -35,10 +36,10 @@ export default function UserAccount() {
                     <img src={avatar} alt="useravatar"></img>
                     <input className="avatar-btn" type="file" accept="image/*"  onChange={handleAvatarChange} />
                 </div>
+                <div>User Name</div>
                 <div className="user-acc-wrapper_container__userinfo">
                 <h1>User Information</h1>
-                <textarea  rows={14} cols={170}></textarea>
-               
+                <textarea  rows={14} cols={170} value={userDescription} onChange={(event) => setUSerDescription(event.target.value)}></textarea>
                 </div>
                 <button className="user-acc-wrapper_save-btn" onClick={handleSave}>Save</button>
             </div>
