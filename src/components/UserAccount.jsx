1018f37939223452e072b618cfb9edff5be05120
Grenky <demosoft62@gmail.com>
2023-08-14 12:10:28 +0300

    13

--- src/components/UserAccount.jsx
+++ src/components/UserAccount.jsx
@@ -5,10 +5,13 @@ export default function UserAccount() {
 
     const[userName, setUserName] = useState('User Name');
     const[avatar, setAvatar] = useState('../images/useravatar.png');
-    const[userDescription, setUSerDescription] = useState('');
+    const[userDescription, setUserDescription] = useState('');
+    const[editedUserName, setEditedUserName] = useState(userName);
+   
+    const[isEditMode, setIsEditMode] = useState(false);
 
     const handleNameChange = (event) => {
-        setUserName(event.target.value);
+        setEditedUserName(event.target.value);
     };
 
     const handleAvatarChange = (event) => {
@@ -25,9 +28,15 @@ export default function UserAccount() {
     };
 
 
-    const handleSave = () => {
+    const handleSave = () => {   
         console.log('saved');
     };
+    
+    const handleSaveName = () => {
+        setUserName(editedUserName)
+        console.log('is saved', editedUserName);
+    };
+
 
     return(
         <div className="user-acc-wrapper">
@@ -35,11 +44,20 @@ export default function UserAccount() {
                 <div className="user-acc-wrapper_container__userphoto">
                     <img src={avatar} alt="useravatar"></img>
                     <input className="avatar-btn" type="file" accept="image/*"  onChange={handleAvatarChange} />
-                </div>
-                <div>User Name</div>
+                </div>    
+                        <h2>{userName}</h2>
+                <input type='text' onChange={handleNameChange} placeholder="User Name" value={editedUserName} className={isEditMode ? 'edit-mode' : ''}></input>
+                {isEditMode ? (
+                    <button onClick={() => setIsEditMode(true)}>Edit</button>
+                ) : ( 
+                    <>
+                    <button onClick={handleSaveName}>edit</button>
+                    <button onClick={() => setIsEditMode(false)}>Cancel</button>
+                    </>
+                )}
                 <div className="user-acc-wrapper_container__userinfo">
                 <h1>User Information</h1>
-                <textarea  rows={14} cols={170} value={userDescription} onChange={(event) => setUSerDescription(event.target.value)}></textarea>
+                <textarea  rows={14} cols={170} value={userDescription} onChange={(event) => setUserDescription(event.target.value)}></textarea>
                 </div>
                 <button className="user-acc-wrapper_save-btn" onClick={handleSave}>Save</button>
             </div>
